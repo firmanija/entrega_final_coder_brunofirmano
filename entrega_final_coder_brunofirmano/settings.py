061e46fc53ff489a92e12491ca59a73904400ea5
Bruno Firmano <bruno.firmano@gmail.com>
2026-03-06 18:25:29 -0300

    chequeo final

--- entrega_final_coder_brunofirmano/settings.py
+++ entrega_final_coder_brunofirmano/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g2bg@*p_^u33l&cf-5b-qco74_r%g60du#65vqo&5vfvkvq9_3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
